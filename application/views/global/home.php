954eb8900fe1c9c987af3dcb1aa202a0696f925d
Adamhudayanto <adamhuda36@gmail.com>
2020-08-10 20:04:27 +0700

    10Agustus2020

---
+++ application/views/global/home.php
@@ -0,0 +1,43 @@
+<div class="row">
+	<div class="col m 12">
+		<center>
+			<h5>Selamat Datang Di Perpustakaan SD Muhammadiyah Blawong 1</h5>
+		</center>
+		<hr>
+	</div>
+
+	<div class="row">
+		<div class="col m12">
+			<div class="card-panel teal darken-2 center white-text">
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus qui iure, inventore repudiandae dolor velit. Tempore, magni possimus officia nobis quam voluptatem fuga officiis aliquam molestiae, quas corrupti necessitatibus consectetur libero doloremque! Quod corporis eaque, eveniet qui distinctio omnis reprehenderit excepturi sunt praesentium aliquam repellat similique doloremque animi dolorem ullam.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col m6 s12">
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img src="<?php echo base_url(); ?>assets2/img/bb.jpg"> <!-- random image -->
+				</div>
+				<div class="card-content teal center">
+					<a class="btn" href="<?php echo base_url(); ?>Buku/profile">Daftar Buku</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col m6 s12">
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img src="<?php echo base_url(); ?>assets2/img/sdmuhblawong1.jpeg"> <!-- random image -->
+				</div>
+				<div class="card-content teal center">
+					<a class="btn" href="<?php echo base_url(); ?>Buku/list_buku">Profile Perpustakaan</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+</div>
---
+++ application/views/global/profile.php
@@ -0,0 +1,40 @@
+<div class="row">
+	<div class="col m 12">
+		<h5>Profil Perpustakaan SD MUBLASA</h5>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col m6">
+		<div class="card-panel teal darken-2 white-text">
+			<h6><b>Visi</b></h6>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni natus est, iste quae at deleniti, animi
+				maiores minus explicabo corporis cupiditate, reiciendis non facilis quasi assumenda laudantium. Ab,
+				molestiae odio!
+			</p><br>
+			<h6><b>Misi</b></h6>
+			<li>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, dolore.
+			</li>
+			<li>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid magnam assumenda ipsa praesentium.
+			</li>
+			<li>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+			</li>
+			<li>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, deleniti distinctio.
+			</li>
+		</div>
+	</div>
+	<div class="col m6">
+		<div class="card-panel teal darken-2 white-text">
+			<h6><b>Identitas Perpustakaan</b></h6>
+			<hr>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum eveniet quod atque animi architecto ad distinctio, magnam eaque non dicta commodi omnis reiciendis libero quaerat vel saepe suscipit voluptate excepturi fuga voluptatibus ut. Molestiae atque quis in rem omnis reiciendis facilis necessitatibus, enim inventore libero provident perferendis eius impedit nostrum magni voluptatem quia tempore nobis ipsum excepturi consequatur adipisci sequi repellat tenetur! Nam dolores perferendis, culpa beatae accusantium nemo, nostrum laboriosam quidem eos aspernatur saepe delectus atque iure, voluptatibus id officiis voluptatem fuga nesciunt. Beatae harum sint pariatur.
+			</p>
+		</div>
+	</div>
+</div>
